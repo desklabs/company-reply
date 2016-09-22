a10859111c78c205ee89653cd7969fa877002083
Thomas Stachl <thomas@stachl.me>
2016-09-22 10:31:31 -0700

    Some logging.

--- web.rb
+++ web.rb
@@ -4,12 +4,14 @@ require 'desk_api'
 
 post '/reply' do
   i = params['interaction']
-  DeskApi.by_url("/api/v2/cases/#{i['ticket_id']}/replies").create({
+  puts i.inspect
+  resp = DeskApi.by_url("/api/v2/cases/#{i['ticket_id']}/replies").create({
     body: i['body'],
     direction: 'in',
     status: 'sent',
     to: ENV['SUPPORT_EMAIL'],
     from: i['customer_email']
   })
+  puts resp.inspect
   redirect request.referer
 end
