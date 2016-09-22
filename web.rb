5f76abfc3925d233b688877120cda7d3377c17b7
Thomas Stachl <thomas@stachl.me>
2016-09-22 10:36:10 -0700

    Set `hidden_at` to `nil`

--- web.rb
+++ web.rb
@@ -4,14 +4,13 @@ require 'desk_api'
 
 post '/reply' do
   i = params['interaction']
-  puts i.inspect
-  resp = DeskApi.by_url("/api/v2/cases/#{i['ticket_id']}/replies").create({
+  DeskApi.by_url("/api/v2/cases/#{i['ticket_id']}/replies").create({
     body: i['body'],
     direction: 'in',
     status: 'sent',
     to: ENV['SUPPORT_EMAIL'],
-    from: i['customer_email']
+    from: i['customer_email'],
+    hidden_at: nil
   })
-  puts resp.inspect
   redirect request.referer
 end
